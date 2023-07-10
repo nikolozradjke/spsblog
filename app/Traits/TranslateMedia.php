375add7afbb9855e39ee5f8179138df783e218f5
Nikoloz Kemularia <nkemularia@dga.gov.ge>
2023-07-10 16:25:20 +0400

    Adding function() - Remove old files in contact info

--- app/Traits/TranslateMedia.php
+++ app/Traits/TranslateMedia.php
@@ -13,11 +13,11 @@ trait TranslateMedia {
         $destination = strtolower(substr(strrchr(self::$current_class, "\\"), 1));
         foreach($this->getLocales() as $lang){
             foreach($this->translate_media as $column_name){
+                $old = $item->content()->where('lang', $lang)->first()->$column_name;
                 if(isset($request->translates[$lang][$column_name])){
-                    $file = app(File::class)->uploadFile($request->translates[$lang][$column_name], $destination);
+                    $file = app(File::class)->uploadFile($request->translates[$lang][$column_name], $destination, $old);
                     $this->translate_media_files[$column_name][$lang] = $file;
                 }else{
-                    $old = $item->content()->where('lang', $lang)->first()->$column_name;
                     $this->translate_media_files[$column_name][$lang] = $old;
                 }
             }
